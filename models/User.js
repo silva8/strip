7b30c0e9af72b8623c221c7e0a67991af1c73c29
Cristobal Silva <silva8@MBPdeCristobal.modem>
2020-01-17 16:08:01 +1100

    User register,login and car register validition files created

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const UserSchema = new Schema({
-	name: {
+    name: {
     	type: String,
     	required: true
   	},
---
+++ validation/carRegister.js
@@ -0,0 +1,41 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateCarRegisterInput(data) {
+  let errors = {};
+// Convert empty fields to an empty string so we can use validator functions
+  data.brand = !isEmpty(data.brand) ? data.brand : "";
+  data.model = !isEmpty(data.model) ? data.model : "";
+  data.year = !isEmpty(data.year) ? data.year : "";
+  data.plate = !isEmpty(data.plate) ? data.plate : "";
+  data.color = !isEmpty(data.color) ? data.color : "";
+  data.seats = !isEmpty(data.seats) ? data.seats : "";
+// Brand checks
+  if (Validator.isEmpty(data.brand)) {
+    errors.brand = "Brand field is required";
+  }
+// Model checks
+  if (Validator.isEmpty(data.model)) {
+    errors.model = "Model field is required";
+  }
+// Year checks
+  if (Validator.isEmpty(data.year)) {
+    errors.year = "Year field is required";
+  }
+// Plate checks
+  if (Validator.isEmpty(data.plate)) {
+    errors.plate = "Plate field is required";
+  }
+// Color checks
+  if (Validator.isEmpty(data.color)) {
+    errors.color = "Color field is required";
+  }
+// Seats checks
+  if (Validator.isEmpty(data.seats)) {
+    errors.seats = "Number of seats is required";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
