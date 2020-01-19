7997f7bc01f584f9dd628436fb8df0621472d68f
Cristobal Silva <silva8@MBPdeCristobal.modem>
2020-01-19 11:24:33 +1100

    Trip validation created, register and login api routes created, passport api configurated

---
+++ validation/newTrip.js
@@ -0,0 +1,31 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+module.exports = function validateNewTripInput(data) {
+  let errors = {};
+// Convert empty fields to an empty string so we can use validator functions
+  data.from = !isEmpty(data.from) ? data.from : "";
+  data.to = !isEmpty(data.to) ? data.to : "";
+  data.date = !isEmpty(data.date) ? data.date : "";
+  data.availableSeats = !isEmpty(data.availableSeats) ? data.availableSeats : "";
+// From checks
+  if (Validator.isEmpty(data.from)) {
+    errors.from = "Starting location is required";
+  }
+// To checks
+  if (Validator.isEmpty(data.to)) {
+    errors.to = "Destination is required";
+  }
+// Date checks
+  if (Validator.isEmpty(data.date)) {
+    errors.date = "Date is required";
+  }
+// Available seats checks
+  if (Validator.isEmpty(data.availableSeats)) {
+    errors.availableSeats = "Numbre of available seats is required";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
